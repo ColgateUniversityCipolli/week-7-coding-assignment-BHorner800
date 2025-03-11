cd2463fc39b3f681830eed01bd372609aa41ae96
BHorner800 <benjamin.r.horner@gmail.com>
2025-03-10 22:15:44 -0400

    Create HW7Code.R

---
+++ HW7Code.R
@@ -0,0 +1,38 @@
+############################################################
+#HW7
+#Ben Horner
+#MATH 240
+############################################################
+############################################################
+#Libraries
+############################################################
+
+
+
+############################################################
+#Question 1: Poisson probability function
+############################################################
+# Use dpois and ppois to conditionally return the correct probability
+pois.prob = function(x, size, prob, type = "<="){
+  '
+  input -> x (lambda), size, probability, type
+  output -> 
+  computes: P(X=x), P(X!=x), P(X<x), P(X<=x), P(X>x), or P(X>=x)
+  default is P(X<=x)
+  '
+  if (type == "="){
+    
+  }
+  if (type == "!="){
+  }
+  if (type == "<"){
+  }
+  if (type ==  "<="){
+  }
+  if (type == ">"){
+  }
+  if (type == "<="){
+  }
+}
+
+?dpois
